a25a4495a3038e0e5966476085cb1791c1d76313
vaniairnanda <vaniairnanda@gmail.com>
2020-02-22 19:05:04 +0700

    add instrument and test done

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,5 @@ const authentication = require("../middlewares/authentication");
 router.post("/login", UserController.login);
 router.use(authentication);
 router.get("/recommendations", UserController.getRecommendedUser);
-
+router.patch("/instruments", UserController.addInstruments);
 module.exports = router;

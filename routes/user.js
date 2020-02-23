441bfd670a98db6801063fd3160207e82bcea40f
Gabriel Timothy <biel.jaeger03@gmail.com>
2020-02-23 17:31:56 +0700

    Get current user info

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const authentication = require("../middlewares/authentication");
 
 router.post("/login", UserController.login);
 router.use(authentication);
+router.get("/me", UserController.getUserDetails);
 router.get("/recommendations", UserController.getRecommendedUser);
 router.patch("/instruments", UserController.addInstruments);
 module.exports = router;

8f1b31e228093137875f4cf035a15c1b829d5d32
vaniairnanda <vaniairnanda@gmail.com>
2020-02-24 13:32:07 +0700

    dummy generator

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,8 @@ const { UserController } = require("../controllers/index");
 const authentication = require("../middlewares/authentication");
 
 router.post("/login", UserController.login);
-router.use(authentication);
+router.post("/dummies", UserController.generateDummies);
+// router.use(authentication);
 router.get("/me", UserController.getUserDetails);
 router.get("/recommendations", UserController.getRecommendedUser);
 router.patch("/instruments", UserController.addInstruments);

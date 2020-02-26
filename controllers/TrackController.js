b95333559371167cc529c11d8e188a006dbcfbf6
Gabriel Timothy <biel.jaeger03@gmail.com>
2020-02-26 14:58:00 +0700

    Add Track is live

--- controllers/TrackController.js
+++ controllers/TrackController.js
@@ -1,21 +1,43 @@
 const { Track } = require("../models/index");
+const fs = require("fs");
+const bucketName = process.env.CLOUD_BUCKET;
+const filepath = "./uploads/";
+const { Storage } = require("@google-cloud/storage");
+const storage = new Storage();
 
 class TrackController {
-  static upload(req, res, next) {
-    Track.create({
-      instrument: req.body.instrument,
-      userId: req.currentUserId,
-      roomId: req.params.roomId,
-      file_path: req.file.cloudStoragePublicUrl
-    })
-      .then(result => {
-        /* istanbul ignore next */
-        req.socket.broadcast.emit("new_track", result);
-      })
-      .catch(err => {
-        /* istanbul ignore next */
-        next(err);
+  static async upload(req, res, next) {
+    let filename = `${req.body.instrument}-${new Date()}-${
+      req.currentUserId
+    }.mp3`;
+    try {
+      let bitmap = Buffer.from(
+        req.body.track.replace("data:audio/mp3;base64,", ""),
+        "base64"
+      );
+      fs.writeFileSync(`./uploads/${filename}`, bitmap);
+      await storage.bucket(bucketName).upload(filepath + filename, {
+        gzip: true,
+        metadata: {
+          cacheControl: "public, max-age=31536000"
+        }
       });
+      fs.unlinkSync(`${process.cwd()}/uploads/${filename}`);
+      console.log("Upload Success");
+      const result = await Track.create({
+        instrument: req.body.instrument,
+        userId: req.currentUserId,
+        roomId: req.params.roomId,
+        file_path: `https://storage.googleapis.com/${bucketName}/${filename}`
+      });
+      req.socket.broadcast.emit(
+        `new_track_upload_${req.params.roomId}`,
+        result
+      );
+      res.status(201).json(result);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   static delete(req, res, next) {
